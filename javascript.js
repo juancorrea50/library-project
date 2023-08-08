fc440246a67adbbcce0df38699a4da8f2f648b12
Juan Correa <juancorrea50@yahoo.com>
2023-08-08 12:47:03 -0500

    Add functionality to new book button
    Pops a form into the html doc to fill out a new book object.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-const newBook = document.querySelector('.new-book')
-
+const newBookButton = document.querySelector('.new-book')
+//default the new book button to have no display style
+document.getElementById('book-input').style.display = 'none'
 
 //Library Array
 let myLibrary = [];
@@ -13,6 +14,23 @@ function Book(author, title, numPages, isRead) {
 }
 
 //Book object function to add to library array
-function addBookToLibrary(){
-
+function addBookToLibrary(...args){
+    /*Push book object into library array */
+    myLibrary.push(args);
+    //Log myLibrary array
+    console.log(myLibrary);
 }
+function createNewBook(){
+    /*Create new book object to enter into the addBookToLibrary function */
+}
+//Open the form to fill out new book object in html doc
+function inputBookInfo(){
+    
+    if(document.getElementById('book-input').style.display == 'none'){
+        document.getElementById('book-input').style.display = 'flex';
+    } else {
+        document.getElementById('book-input').style.display = 'none';
+    }
+}
+//click listener to pop form into html doc
+newBookButton.addEventListener('click', inputBookInfo);
